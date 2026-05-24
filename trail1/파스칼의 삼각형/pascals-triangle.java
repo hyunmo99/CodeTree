9d66cb85793f86f0fbddcdcf3016e359dd0bb8f2
hyunmo99 <lsiz8297@gmail.com>
2026-05-24 21:40:15 +0900

    [Passed] 파스칼의 삼각형 (93ms, 9MB)

---
+++ trail1/파스칼의 삼각형/pascals-triangle.java
@@ -0,0 +1,36 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+  public static void main(String[] args) throws IOException {
+
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+    StringTokenizer st = new StringTokenizer(br.readLine());
+    int N = Integer.parseInt(st.nextToken());
+
+    int[][] arr = new int[N][N];
+
+    for (int i = 0; i < N; i++) {
+      arr[i][0] = 1;
+      arr[i][i] = 1;
+    }
+    for (int i = 2; i < N; i++) {
+      for (int j = 1; j < i; j++) {
+        arr[i][j] = arr[i-1][j-1] + arr[i-1][j];
+      }
+    }
+
+    for (int i = 0; i < N; i++) {
+      for (int j = 0; j < N; j++) {
+        if (arr[i][j] != 0) {
+          System.out.print(arr[i][j] + " ");
+        }
+      }
+      System.out.println();
+    }
+  }
+
+}
