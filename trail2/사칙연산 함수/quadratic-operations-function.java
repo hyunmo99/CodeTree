945ffc2b2ce0549a596af1a2bb93b8ef109903dd
hyunmo99 <lsiz8297@gmail.com>
2026-05-25 01:58:01 +0900

    [Passed] 사칙연산 함수 (87ms, 9MB)

---
+++ trail2/사칙연산 함수/quadratic-operations-function.java
@@ -0,0 +1,47 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+  public static void main(String[] args) throws IOException {
+
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+    StringTokenizer st = new StringTokenizer(br.readLine());
+
+    int a =  Integer.parseInt(st.nextToken());
+    char o = st.nextToken().charAt(0);
+    int c =  Integer.parseInt(st.nextToken());
+
+    String res = calculator(a, o, c);
+    System.out.println(res);
+
+  }
+
+  private static String calculator(int a, char o, int c) {
+    int res = 0;
+    switch (o) {
+      case '+':
+        res = a + c;
+        break;
+
+      case '-':
+        res = a - c;
+        break;
+
+      case '*':
+        res = a * c;
+        break;
+
+      case '/':
+        res = a / c;
+        break;
+
+      default:
+        return "False";
+    }
+    return String.format("%d %s %d = %d", a, o, c, res);
+    
+  }
+}
