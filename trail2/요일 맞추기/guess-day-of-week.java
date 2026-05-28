c3ac08007102e884b8cf1fb53d308671423a3715
hyunmo99 <lsiz8297@gmail.com>
2026-05-29 00:15:42 +0900

    [Passed] 요일 맞추기 (89ms, 9MB)

---
+++ trail2/요일 맞추기/guess-day-of-week.java
@@ -0,0 +1,40 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+  static int[] numOfDays = new int[]{0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+
+
+  public static void main(String[] args) throws IOException {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    StringTokenizer st = new StringTokenizer(br.readLine());
+
+    int m1 = Integer.parseInt(st.nextToken());
+    int d1 = Integer.parseInt(st.nextToken());
+    int m2 = Integer.parseInt(st.nextToken());
+    int d2 = Integer.parseInt(st.nextToken());
+
+    String[] dayOfTheWeek = new String[]{"Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"};
+
+    int start = getDayOfYear(m1, d1);
+    int end = getDayOfYear(m2, d2);
+    int differ = end - start;
+
+    int index = ((differ % 7) + 7) % 7;
+
+    System.out.println(dayOfTheWeek[index]);
+  }
+
+  private static int getDayOfYear(int m1, int d1) {
+    int num = 0;
+
+    for (int i = 1; i < m1; i++) {
+      num += numOfDays[i];
+    }
+
+    return num + d1;
+  }
+}
